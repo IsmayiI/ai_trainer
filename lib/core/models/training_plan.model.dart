9696a3c8534ea904dc21073827c8040c0285d656
Ismayil <ismayilov_ismayil_97@mail.ru>
2025-12-30 13:36:40 +0400

    create training_plan model

---
+++ lib/core/models/training_plan.model.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'training_plan.model.freezed.dart';
+
+@freezed
+sealed class TrainingPlan with _$TrainingPlan {
+  const factory TrainingPlan({
+    required String title,
+    required DateTime startDate,
+    required List<PlanItem> plans,
+  }) = _TrainingPlan;
+}
+
+@freezed
+sealed class PlanItem with _$PlanItem {
+  const factory PlanItem({
+    required String exercise,
+    required num sets,
+    required List<num> reps,
+    required List<num> weight,
+  }) = _PlanItem;
+}
